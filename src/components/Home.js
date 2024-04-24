46e01101c33a941ad700e1b3ac1644203eba06f2
imani <18imani.vata@gmail.com>
2024-04-24 20:35:11 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
+import {username, city} from "../data/user.js";
 
-function Home() {
+
+export default function Home() {
   return (
     <div id="home">
       <h1>
@@ -9,3 +11,6 @@ function Home() {
     </div>
   );
 }
+
+
+
